9659678317e580dff0059f905262baac0012b2e4
xavierllabres1 <xavierllabres1@paucasesnovescifp.cat>
2023-02-03 18:44:29 +0100

    ipc close

--- js/ipc.js
+++ js/ipc.js
@@ -1,9 +1,15 @@
-const { net, ipcMain } = require('electron')
+const { net, ipcMain, BrowserWindow } = require('electron')
 
+// Close
+ipcMain.on('close_window', (e, args) => {
+
+  const login = BrowserWindow.fromWebContents(e.sender);
+  login.close();
+
+})
 
 // GET all Allotjaments
 ipcMain.on('get_allotjaments', (e, args) => {
-
   const valueRequest = {
     method: 'GET',
     protocol: 'http:',
@@ -18,32 +24,30 @@ ipcMain.on('get_allotjaments', (e, args) => {
       e.sender.send('get_allotjaments', JSON.parse(chunk))
       //console.log(`BODY: ${chunk}`)
     })
+  })
+  request.end()
+})
+
+// POST Login
+ipcMain.on('check_user', (e, args) => {
+
+  const { net } = require('electron')
+  const valueRequest = {
+      method: 'GET',
+      protocol: 'http:',
+      hostname: 'etv.dawpaucasesnoves.com/etvServidor/public',
+      // port:'80',
+      path: '/api/login'
+  }
+  const request = net.request(valueRequest);
+  request.on('response', (response) => {
+
+          // 
+          e.sender.send('check_user', response)
 
   })
   request.end()
-
-})
-
-//GET Login
-// ipcMain.on('check_user', (e, args) => {
-
-//   const { net } = require('electron')
-//   const valueRequest = {
-//       method: 'GET',
-//       protocol: 'http:',
-//       hostname: 'etv.dawpaucasesnoves.com/etvServidor/public',
-//       // port:'80',
-//       path: '/api/login'
-//   }
-//   const request = net.request(valueRequest);
-//   request.on('response', (response) => {
-
-//           // 
-//           e.sender.send('check_user', response)
-
-//   })
-//   request.end()
   
-// })
+})
 
 //console.log("EXTRACT IPC WORKS");
