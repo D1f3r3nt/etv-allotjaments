de3eb59d79c31b976f529ec8a3efdf77b13557f1
D1f3r3nt <98466831+D1f3r3nt@users.noreply.github.com>
2023-02-03 16:25:19 +0100

    init

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        height: 600,
+        width: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true
+        },
+        title: 'ETV Allotjaments',
+    });
+
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
