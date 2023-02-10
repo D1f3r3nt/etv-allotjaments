7c87867a8e39f3ec7a75a4ae9346bf98af7e4f4e
D1f3r3nt <98466831+D1f3r3nt@users.noreply.github.com>
2023-02-10 18:00:46 +0100

    upload CDNS

--- pages/scripts/index.js
+++ pages/scripts/index.js
@@ -6,64 +6,30 @@ const cards = $('.cards')
 
 $(() => {
 
-    ipcRenderer.send('get_allotjaments');
+  for (let index = 0; index < 10; index++) {
+    cards.append(CardIndex({
+      id: 1,
+      image: 'https://loremflickr.com/300/225/house',
+      name: 'nombre',
+      address: 'adresa',
+      description: 'descripcio'
+    }))
 
-    ipcRenderer.on('res_get_allotjaments', (_, data) => {
-        let response = data.result.data;
+  }
 
-        response.map(dato => {
-            cards.append(CardIndex({
-                image: 'https://loremflickr.com/300/225/house',
-                name: dato.nombre,
-                address: dato.direccion,
-                description: dato.descripcion
-            }))
-        });
-    })
+  //ipcRenderer.send('get_allotjaments');
 
-});
+  ipcRenderer.on('res_get_allotjaments', (_, data) => {
+    let response = data.result.data;
 
-$(document).ready(function(){
-  var zindex = 10;
-  
-  $("div.card").click(function(e){
-    e.preventDefault();
-
-    var isShowing = false;
-
-    if ($(this).hasClass("show")) {
-      isShowing = true
-    }
-
-    if ($("div.cards").hasClass("showing")) {
-      // a card is already in view
-      $("div.card.show")
-        .removeClass("show");
-
-      if (isShowing) {
-        // this card was showing - reset the grid
-        $("div.cards")
-          .removeClass("showing");
-      } else {
-        // this card isn't showing - get in with it
-        $(this)
-          .css({zIndex: zindex})
-          .addClass("show");
-
-      }
-
-      zindex++;
-
-    } else {
-      // no cards in view
-      $("div.cards")
-        .addClass("showing");
-      $(this)
-        .css({zIndex:zindex})
-        .addClass("show");
-
-      zindex++;
-    }
-    
+    response.map(dato => {
+      cards.append(CardIndex({
+        id: data.ID,
+        image: 'https://loremflickr.com/300/225/house',
+        name: dato.nombre,
+        address: dato.adresa,
+        description: dato.descripcio
+      }))
+    });
   });
 });
